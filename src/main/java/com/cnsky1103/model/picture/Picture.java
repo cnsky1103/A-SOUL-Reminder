8307a96cfb34a68fd0f816c77393ebc6cf40803d
cnsky1103 <cnsky1103@gmail.com>
2021-11-06 20:34:29 +0800

    get random picture

---
+++ src/main/java/com/cnsky1103/model/picture/Picture.java
@@ -0,0 +1,84 @@
+package com.cnsky1103.model.picture;
+
+import java.util.List;
+
+public class Picture {
+    private String img;
+    private String dy_url;
+    private List<Tag> tags;
+    private Owner owner;
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getDy_url() {
+        return dy_url;
+    }
+
+    public void setDy_url(String dy_url) {
+        this.dy_url = dy_url;
+    }
+
+    public List<Tag> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<Tag> tags) {
+        this.tags = tags;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public static class Tag {
+        private String tag_id;
+        private String tag_title;
+
+        public String getTag_id() {
+            return tag_id;
+        }
+
+        public void setTag_id(String tag_id) {
+            this.tag_id = tag_id;
+        }
+
+        public String getTag_title() {
+            return tag_title;
+        }
+
+        public void setTag_title(String tag_title) {
+            this.tag_title = tag_title;
+        }
+    }
+
+    public static class Owner {
+        private String name;
+        private String uid;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getUid() {
+            return uid;
+        }
+
+        public void setUid(String uid) {
+            this.uid = uid;
+        }
+    }
+}
---
+++ src/main/java/com/cnsky1103/service/Pictures.java
@@ -0,0 +1,62 @@
+package com.cnsky1103.service;
+
+import com.cnsky1103.model.picture.Picture;
+import com.google.gson.Gson;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+
+public class Pictures {
+    public static Picture getRandomPicture() throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException, IOException {
+        // use the TrustSelfSignedStrategy to allow Self Signed Certificates
+        SSLContext sslContext = SSLContextBuilder
+                .create()
+                .loadTrustMaterial(new TrustSelfSignedStrategy())
+                .build();
+
+        // we can optionally disable hostname verification.
+        // if you don't want to further weaken the security, you don't have to include this.
+        HostnameVerifier allowAllHosts = new NoopHostnameVerifier();
+
+        // create an SSL Socket Factory to use the SSLContext with the trust self signed certificate strategy
+        // and allow all hosts verifier.
+        SSLConnectionSocketFactory connectionFactory = new SSLConnectionSocketFactory(sslContext, allowAllHosts);
+
+        // finally create the HttpClient using HttpClient factory methods and assign the ssl socket factory
+        HttpClient client = HttpClients
+                .custom()
+                .setSSLSocketFactory(connectionFactory)
+                .build();
+
+
+        //HttpClientBuilder.create().build();
+        HttpGet request = new HttpGet("https://api.asoul.cloud:8000/getRandomPic");
+        HttpResponse response = client.execute(request);
+        BufferedReader rd = new BufferedReader(
+                new InputStreamReader(response.getEntity().getContent()));
+
+        StringBuffer result = new StringBuffer();
+        String line;
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
+        }
+        System.out.println(result);
+        Gson pictureGson = new Gson();
+        Picture picture = pictureGson.fromJson(result.toString(), Picture.class);
+        return picture;
+    }
+}
