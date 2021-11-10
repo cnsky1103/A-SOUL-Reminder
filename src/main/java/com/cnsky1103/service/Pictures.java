eef523d1d1f92edeaad548fe74ef30bf30de52f6
cnsky1103 <cnsky1103@gmail.com>
2021-11-10 16:32:49 +0800

    完善了随机二创图片的获取和保存

--- src/main/java/com/cnsky1103/service/Pictures.java
+++ src/main/java/com/cnsky1103/service/Pictures.java
@@ -1,8 +1,10 @@
 package com.cnsky1103.service;
 
+import com.cnsky1103.config.Path;
 import com.cnsky1103.model.picture.Picture;
 import com.google.gson.Gson;
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.conn.ssl.NoopHostnameVerifier;
@@ -13,15 +15,79 @@ import org.apache.http.ssl.SSLContextBuilder;
 
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLContext;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
+import java.io.*;
 
+/**
+ * 图片的获取、下载
+ * @author sky
+ */
 public class Pictures {
-    public static Picture getRandomPicture() throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException, IOException {
+    /**
+     * 随机获取一张二创图片
+     * @return Picture
+     * @see com.cnsky1103.model.picture.Picture
+     * @throws Exception
+     */
+    public static Picture getRandomPicture() throws Exception {
+        HttpClient client = getHttpClient();
+
+        HttpGet request = new HttpGet(Path.randomPictureUrl);
+        HttpResponse response = client.execute(request);
+        BufferedReader rd = new BufferedReader(
+                new InputStreamReader(response.getEntity().getContent()));
+
+        StringBuffer result = new StringBuffer();
+        String line;
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
+        }
+        System.out.println(result);
+        Gson pictureGson = new Gson();
+        Picture picture = pictureGson.fromJson(result.toString(), Picture.class);
+        return picture;
+    }
+
+    /**
+     * 将图片下载到本地
+     * @param url 图片的源地址
+     * @return 下载到本地的路径
+     * @throws Exception
+     */
+    public static String downloadPicture(String url) throws Exception {
+        HttpClient client = getHttpClient();
+        HttpGet request = new HttpGet(url);
+        HttpResponse response = client.execute(request);
+        if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+            InputStream in = response.getEntity().getContent();
+            File projectDirFile = new File(Path.pluginImgPath);
+            if (!projectDirFile.exists()) {
+                projectDirFile.mkdir();
+            }
+            String imgPath = projectDirFile + File.separator + "img.jpg";
+            System.out.println(imgPath);
+            File imgFile = new File(imgPath);
+            if (!imgFile.exists()) {
+                imgFile.createNewFile();
+            }
+
+            FileOutputStream fos = new FileOutputStream(imgFile);
+            byte[] buffer = new byte[1024];
+            int len = 0;
+            while ((len = in.read(buffer)) != -1) {
+                fos.write(buffer, 0, len);
+            }
+            fos.flush();
+            fos.close();
+            return imgPath;
+        } else {
+            throw new Exception("download error");
+        }
+    }
+
+    /**
+     * 绕过SSL证书
+     */
+    private static HttpClient getHttpClient() throws Exception {
         // use the TrustSelfSignedStrategy to allow Self Signed Certificates
         SSLContext sslContext = SSLContextBuilder
                 .create()
@@ -42,21 +108,6 @@ public class Pictures {
                 .setSSLSocketFactory(connectionFactory)
                 .build();
 
-
-        //HttpClientBuilder.create().build();
-        HttpGet request = new HttpGet("https://api.asoul.cloud:8000/getRandomPic");
-        HttpResponse response = client.execute(request);
-        BufferedReader rd = new BufferedReader(
-                new InputStreamReader(response.getEntity().getContent()));
-
-        StringBuffer result = new StringBuffer();
-        String line;
-        while ((line = rd.readLine()) != null) {
-            result.append(line);
-        }
-        System.out.println(result);
-        Gson pictureGson = new Gson();
-        Picture picture = pictureGson.fromJson(result.toString(), Picture.class);
-        return picture;
+        return client;
     }
 }
